4c90c19cf08a5f4fc2a65f33a593d2c62ff9ae9a
Shehan Wijeyaweera <shehan.naveendra@gmail.com>
2020-08-23 13:29:11 +0530

    implemented new book class and repository without book controller

--- src/main/java/com/thymeleaf/onlinebookstore/thymeleafOnlineBookstore/repository/StudentRepository.java
+++ src/main/java/com/thymeleaf/onlinebookstore/thymeleafOnlineBookstore/repository/StudentRepository.java
@@ -3,12 +3,13 @@ package com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.repository;
 import java.util.List;
 
 import com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 
 @Repository
-public interface StudentRepository extends CrudRepository <Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findByName(String name);
 }
--- src/main/java/com/thymeleaf/onlinebookstore/thymeleafOnlineBookstore/repository/UserRepository.java
+++ src/main/java/com/thymeleaf/onlinebookstore/thymeleafOnlineBookstore/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.repository;
 
 import com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
