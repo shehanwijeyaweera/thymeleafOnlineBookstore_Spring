199aab1c426911a3648e73b814609c1e337a8830
Shehan Wijeyaweera <shehan.naveendra@gmail.com>
2020-09-21 17:02:08 +0530

    Added a new template for customer side. implemented a new function to redirect to homepage by checking the user role. created new fragment file for new template.

--- src/main/java/com/thymeleaf/onlinebookstore/thymeleafOnlineBookstore/controller/loginController.java
+++ src/main/java/com/thymeleaf/onlinebookstore/thymeleafOnlineBookstore/controller/loginController.java
@@ -1,13 +1,48 @@
 package com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.controller;
 
+import com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.model.User;
+import com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class loginController {
 
+
     @GetMapping("/login")
     public String login(){
         return "login";
+    }
+
+    @GetMapping("/homepage_cust")
+    public String cust_homepage(){
+        return "Customer_homepage";
+    }
+
+    @GetMapping("/")
+    public String pageRedirection(Model model)
+    {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication.getAuthorities().stream().anyMatch(r -> r.getAuthority().equals("Admin")))
+        {
+            return "index";
+        }
+        if (authentication.getAuthorities().stream().anyMatch(r -> r.getAuthority().equals("User")))
+        {
+            return "Customer_homepage";
+        }
+        if(authentication.getAuthorities().stream().anyMatch(r -> r.getAuthority().equals("Storeworker")))
+        {
+
+        }
+
+        return "index";
     }
 }
