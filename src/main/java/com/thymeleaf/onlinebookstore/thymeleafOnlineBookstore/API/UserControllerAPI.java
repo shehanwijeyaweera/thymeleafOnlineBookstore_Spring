e626e871b4edd98bbfd76bc311454ca66c43096d
Shehan Wijeyaweera <shehan.naveendra@gmail.com>
2020-11-14 23:42:36 +0530

    API login function with Json

--- src/main/java/com/thymeleaf/onlinebookstore/thymeleafOnlineBookstore/API/UserControllerAPI.java
+++ src/main/java/com/thymeleaf/onlinebookstore/thymeleafOnlineBookstore/API/UserControllerAPI.java
@@ -7,8 +7,8 @@ import com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.repository.UserRep
 import com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.service.BookService;
 import com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.service.OrdersService;
 import com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.service.UserService;
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -51,18 +51,30 @@ public class UserControllerAPI {
     }
 
     @GetMapping("/login/{username}/{password}")
-    public String login(@PathVariable(value = "username") String username, @PathVariable(value = "password") String password){
+    public JSONObject login(@PathVariable(value = "username") String username, @PathVariable(value = "password") String password){
+        JSONObject obj = new JSONObject();
         User user = new User();
         user = userRepository.findByUsername(username);
         String pass = user.getPassword();
         if(user !=null){
             if(userService.passwordencode(password,pass)){
-                return "true";
+
+                obj.put("username", user.getUsername());
+                obj.put("userRole", user.getUserRole());
+                obj.put("Response", "Correct");
+                return obj;
             }
             else {
-                return "false";
+
+                obj.put("username", user.getUsername());
+                obj.put("userRole", user.getUserRole());
+                obj.put("Response", "Wrong");
+                return obj;
             }
         }
-        return "fuck";
+        obj.put("username", "Error");
+        obj.put("userRole", "Error");
+        obj.put("Response", "Error");
+        return obj;
     }
 }
