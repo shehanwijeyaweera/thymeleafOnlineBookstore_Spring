0fbed627c04d5ad836e11517c30c72c08eb9cdf9
Shehan Wijeyaweera <shehan.naveendra@gmail.com>
2020-12-11 22:28:35 +0530

    user registration API side implemented. email function disabled due to login errors.

--- src/main/java/com/thymeleaf/onlinebookstore/thymeleafOnlineBookstore/API/UserControllerAPI.java
+++ src/main/java/com/thymeleaf/onlinebookstore/thymeleafOnlineBookstore/API/UserControllerAPI.java
@@ -7,10 +7,17 @@ import com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.service.BookServic
 import com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.service.Customer_orderItemsService;
 import com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.service.OrdersService;
 import com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.service.UserService;
+import com.thymeleaf.onlinebookstore.thymeleafOnlineBookstore.web.dto.UserRegistrationDto;
+import net.bytebuddy.utility.RandomString;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -47,45 +54,46 @@ public class UserControllerAPI {
     @Autowired
     private Customer_orderItemsService customer_orderItemsService;
 
+    @Autowired
+    private JavaMailSender mailSender;
 
 
     @GetMapping("/users")
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @GetMapping("/books")
-    public List<Book> getallbooks(){
+    public List<Book> getallbooks() {
         return bookService.getAllBooks();
     }
 
     @GetMapping("/book/{id}")
-    public Book getbookdetails(@PathVariable(value  = "id") Long bookid){
+    public Book getbookdetails(@PathVariable(value = "id") Long bookid) {
         return bookService.findById(bookid);
     }
 
     @GetMapping("/login/{username}/{password}")
-    public JSONObject login(@PathVariable(value = "username") String username, @PathVariable(value = "password") String password){
+    public JSONObject login(@PathVariable(value = "username") String username, @PathVariable(value = "password") String password) {
         JSONObject obj = new JSONObject();
         User user = new User();
         user = userRepository.findByUsername(username);
         String pass = user.getPassword();
-        if(user !=null){
-            if(userService.passwordencode(password,pass)){
+        if (user != null) {
+            if (userService.passwordencode(password, pass)) {
 
                 obj.put("user", user);
                 obj.put("Response", "Correct");
                 obj.put("Role", user.getUserRole());
                 return obj;
-            }
-            else {
+            } else {
 
                 obj.put("username", user.getUsername());
                 obj.put("userRole", user.getUserRole());
                 obj.put("Response", "Wrong");
                 return obj;
             }
-        }else{
+        } else {
             obj.put("username", "Error");
             obj.put("userRole", "Error");
             obj.put("Response", "Error");
@@ -95,7 +103,7 @@ public class UserControllerAPI {
     }
 
     @GetMapping("/admindashboard")
-    public JSONObject dashboardDetails(){
+    public JSONObject dashboardDetails() {
         JSONObject obj = new JSONObject();
         obj.put("listOrders", ordersService.getAllOrders().size());
         obj.put("allcustomers", userRepository.findAll().size());
@@ -108,31 +116,31 @@ public class UserControllerAPI {
     }
 
     @GetMapping("/custorders")
-    public List<Customer_orders> getAllCustomerOrders(){
+    public List<Customer_orders> getAllCustomerOrders() {
         List<Customer_orders> customer_orders = ordersRepository.getAllNewOrders();
         return customer_orders;
     }
 
     @GetMapping("/custpendingorders")
-    public List<Customer_orders> getAllPendingOrders(){
+    public List<Customer_orders> getAllPendingOrders() {
         List<Customer_orders> customer_orders = ordersRepository.getAllPendingOrders();
         return customer_orders;
     }
 
     @GetMapping("/custshippedorders")
-    public List<Customer_orders> getAllShippedOrders(){
+    public List<Customer_orders> getAllShippedOrders() {
         List<Customer_orders> customer_orders = ordersRepository.getAllshippedOrders();
         return customer_orders;
     }
 
     @GetMapping("/customerorders/{userid}")
-    public List<Customer_orders> getCustomerPastOrders(@PathVariable(value = "userid")Long userid){
+    public List<Customer_orders> getCustomerPastOrders(@PathVariable(value = "userid") Long userid) {
         List<Customer_orders> customer_orders = ordersRepository.getCustomerOrders(userid);
         return customer_orders;
     }
 
     @PostMapping("/checkout/{username}/{total}")
-    public JSONObject checkout(@PathVariable(value = "username") String username, @PathVariable( value = "total") double total, @RequestBody List<CartItem> cartItem){
+    public JSONObject checkout(@PathVariable(value = "username") String username, @PathVariable(value = "total") double total, @RequestBody List<CartItem> cartItem) {
 
         String username1 = username;
         double total1 = total;
@@ -154,7 +162,7 @@ public class UserControllerAPI {
         Long orderId = orders.getId();
 
         //add order items
-        for(CartItem cartItem1: cartItem){
+        for (CartItem cartItem1 : cartItem) {
             Customer_orderItems customer_orderItems = new Customer_orderItems();
             customer_orderItems.setBook(cartItem1.getBook());
             customer_orderItems.setCustomer_orders(orders);
@@ -172,13 +180,57 @@ public class UserControllerAPI {
     }
 
     @PostMapping("/newUser/register")
-    public JSONObject registerUser(@RequestBody User user){
-
-
+    public JSONObject registerUser(@RequestBody UserRegistrationDto registrationDto) throws UnsupportedEncodingException, MessagingException {
         JSONObject obj = new JSONObject();
-        obj.put("user", "User");
-        obj.put("Response", "Correct");
+        UserRegistrationDto userRegistrationDto = new UserRegistrationDto();
+        userRegistrationDto = registrationDto;
+        String username = registrationDto.getUsername();
+
+        if (userRepository.findByUsername(username) != null) {
+            obj.put("user", registrationDto);
+            obj.put("Response", "Username Already taken");
+        } else {
+            registrationDto.setUserRole("User");
+            registrationDto.setEnabled(false);
+
+            String randomcode = RandomString.make(64);
+
+            registrationDto.setVerificationCode(randomcode);
+
+            userService.save(registrationDto);
+
+            //sendVerificationEmail(registrationDto);
+
+            obj.put("user", registrationDto);
+            obj.put("Response", "Correct");
+        }
 
         return obj;
+    }
+
+    public void sendVerificationEmail(UserRegistrationDto registrationDto) throws MessagingException, UnsupportedEncodingException {
+        String subject = "Please verify your registration";
+        String senderName = "OnlineBookStore team";
+        String mailContent = "<p>Dear " + registrationDto.getUser_fName() + ",</p>";
+        mailContent += "<p>Please Click the link below to verify to activate your account: </p>";
+        mailContent += "<a>http://localhost:8080/registration/verify/"+ registrationDto.getVerificationCode() +"/"+registrationDto.getUsername()+"</a>";
+        mailContent += "<p>Thank you <br> The OnlineBookStore Team </p>";
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom("onlinebookstorelk@gmail.com", senderName);
+        helper.setTo(registrationDto.getUser_email());
+        helper.setSubject(subject);
+
+        helper.setText(mailContent, true);
+
+        mailSender.send(message);
+    }
+
+    @GetMapping("/search/{keyword}")
+    public List<Book> getSearchResults(@PathVariable(value = "keyword") String keyword) {
+        List<Book> bookList = bookService.findAllSearch(keyword);
+        return bookList;
     }
 }
